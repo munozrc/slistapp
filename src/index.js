0476b999a7ae80e3510a48ee6e976a50795d9a1a
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-03-17 20:07:17 -0500

    styled-components added

--- src/index.js
+++ src/index.js
@@ -1,14 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import { createGlobalStyle } from "styled-components";
+
+import App from "./App";
+
+const GlobalStyle = createGlobalStyle`
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+    sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+`;
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
 
-serviceWorkerRegistration.unregister();
+serviceWorkerRegistration.register();
