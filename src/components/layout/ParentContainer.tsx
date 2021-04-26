bab398b3d5d53e2bd688e333718c5a6ffe409d50
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-04-26 15:42:32 -0500

    create firts layout and parent

---
+++ src/components/layout/ParentContainer.tsx
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: white;
+`;
+const ChildDiv = styled.div`
+  width: 50%;
+  height: 80%;
+  max-width: 375px;
+  max-height: 667px;
+  text-align: center;
+  background-color: white;
+  border-radius: 20px;
+  box-shadow: 0 10px 25px 0 rgb(0 0 0 / 20%);
+`;
+
+type Props = {
+  children?: JSX.Element | JSX.Element[];
+};
+
+export default function ParentContainer({ children }: Props) {
+  return (
+    <Wrapper>
+      <ChildDiv>{children}</ChildDiv>
+    </Wrapper>
+  );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import { createGlobalStyle } from "styled-components";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+
+// Components
+import App from "./App";
+import ParentContainer from "./components/layout/ParentContainer";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  body {
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+    sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+`;
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <GlobalStyle />
+    <ParentContainer>
+      <App />
+    </ParentContainer>
   </React.StrictMode>,
   document.getElementById("root")
 );
