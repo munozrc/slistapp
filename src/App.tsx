7eaf46668c2e6e8501d8b4bc61672fd365ad384f
Carlos Muñoz <munozrengifocarlos@gmail.com>
2021-04-26 15:55:49 -0500

    add MainView to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,16 @@
+import styled from "styled-components";
+import MainView from "./views/MainView";
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: #e4e4e4;
+`;
+
 export default function App() {
-  return <h1>Hello World PWA</h1>;
+  return (
+    <Container>
+      <MainView />
+    </Container>
+  );
 }
