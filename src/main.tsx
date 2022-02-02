6d34a813126f16f743d2e7abde068daca0b1bd23
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-02-02 01:03:51 -0500

    chore: set pwa config

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { registerSW } from 'virtual:pwa-register'
 import { App } from './App'
 import './main.css'
 
@@ -9,3 +10,5 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 )
+
+registerSW()
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa'
+
+const pwaOptions: Partial<VitePWAOptions> = {
+  includeAssets: ['favicon.svg', 'robots.txt'],
+  manifest: {
+    name: 'Shopping List App',
+    short_name: 'Shopping List',
+    theme_color: '#ffffff',
+    start_url: './',
+    icons: [
+      {
+        src: 'logo192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: 'logo512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      }
+    ]
+  }
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react(),
+    VitePWA(pwaOptions)
+  ]
 })
