d06589440a53cd515a0f89e92e39595a8b40740a
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-02-11 16:51:45 -0500

    feat: Add ListProducts section

---
+++ src/ListProducts/ListProducts.tsx
@@ -0,0 +1,11 @@
+import styles from './ListProducts.module.css'
+
+export const ListProducts = () => {
+  return (
+    <section className={styles.listProductsContainer}>
+      <article>
+        <h4>Pan</h4>
+      </article>
+    </section>
+  )
+}
