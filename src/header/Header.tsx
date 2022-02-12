910f72e842d3f88646c674f0c9234f6977e13e32
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-02-11 19:24:26 -0500

    feat: Set view state

--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -1,13 +1,17 @@
 import { Button } from "../components/Button"
 import { BackIcon, SettingsIcon } from "../components/Icons"
+import { useView } from "../hooks/useView"
 import styles from "./Header.module.css"
 
 export const Header = () => {
+  const { view } = useView()
   return (
     <header className={styles.headerContainer}>
-      <Button>
-        <BackIcon />
-      </Button>
+      { view !== "HOME" && (
+        <Button>
+          <BackIcon />
+        </Button>
+      )}
       <h2 className={styles.title}>Inicio</h2>
       <Button>
         <SettingsIcon />
