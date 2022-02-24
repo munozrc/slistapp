355984455a595e1ab6d042daef0a72a177b88011
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-02-23 21:08:34 -0500

    fix: Use app components

--- src/list/ListProducts.tsx
+++ src/list/ListProducts.tsx
@@ -1,5 +1,6 @@
+import { VIEWS } from '../app/contexts'
+import { useView } from '../app/hooks'
 import { PlusIcon } from '../components/Icons'
-import { useView } from '../hooks/useView'
 
 import { ListItem } from './ListItem'
 import styles from './ListProducts.module.css'
@@ -8,7 +9,7 @@ export const ListProducts = () => {
   const { changeView } = useView()
   return (
     <section className={styles.listProductsContainer}>
-      <ListItem onClick={() => changeView('ADD_PRODUCT')}>
+      <ListItem onClick={() => changeView(VIEWS.ADD_PRODUCT)}>
         <p>Nuevo elemento</p>
         <PlusIcon />
       </ListItem>
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import ReactDOM from 'react-dom'
 import { registerSW } from 'virtual:pwa-register'
 
-import { UIContextProvider } from './contexts/UIContext'
-import { View } from './components/View'
+import { UIContextProvider } from './app/contexts'
+import { View } from './app/components'
 import { ListProducts } from './list'
 import { Product } from './product'
 
