30de798ebd06b45872277c5e20cf1eb0a0d0e512
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-02-23 22:45:33 -0500

    feat: use new component ListProducts

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,13 @@ import { registerSW } from 'virtual:pwa-register'
 
 import { UIContextProvider, VIEWS } from './app/contexts'
 import { View } from './app/view'
-import { ListProducts } from './list'
 import { HomeView } from './views'
 
 import './main.css'
 
 ReactDOM.render(
   <UIContextProvider>
-    <View view={VIEWS.HOME} component={<ListProducts />}/>
+    <View view={VIEWS.HOME} component={<HomeView />}/>
     <View view={VIEWS.ADD_PRODUCT} component={<HomeView />}/>
   </UIContextProvider>,
   document.getElementById('root')
--- src/views/HomeView.tsx
+++ src/views/HomeView.tsx
@@ -1,9 +1,10 @@
 import { Layout } from '../components/Layout'
+import { ListProducts } from '../product/components/ListProducts'
 
 export const HomeView = () => {
   return (
     <Layout title="Inicio">
-      <p>Lista de productos</p>
+      <ListProducts />
     </Layout>
   )
 }
