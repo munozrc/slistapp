90d4c5b784d70a153b33905e435957c02019ae62
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-02-24 20:43:27 -0500

    feat: use new form add product

--- src/views/AddProductView.tsx
+++ src/views/AddProductView.tsx
@@ -1,12 +1,20 @@
 import { VIEWS } from '../app/contexts'
 import { useView } from '../app/hooks'
+import { Button } from '../components/Button'
+import { InputField } from '../components/InputField'
 import { Layout } from '../components/Layout'
 
 export const AddProductView = () => {
   const { changeView } = useView()
   return (
-    <Layout align='start' title="Agregar producto" handleClick={() => changeView(VIEWS.HOME)}>
-      <h4>nombre</h4>
+    <Layout align="start" title="Agregar producto" handleClick={() => changeView(VIEWS.HOME)}>
+      <form onSubmit={() => {}}>
+        <InputField type="text" placeholder="Nombre" name="name" />
+        <InputField type="number" placeholder="Precio" name="price" />
+        <div style={{ marginTop: '20px' }}>
+          <Button variant='primary'>Agregar</Button>
+        </div>
+      </form>
     </Layout>
   )
 }
