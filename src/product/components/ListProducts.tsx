0a610d9e0d7df80c58a41b8b2225da256f5be84a
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-02-24 21:48:17 -0500

    feat: map all products with listProducts

--- src/product/components/ListProducts.tsx
+++ src/product/components/ListProducts.tsx
@@ -1,20 +1,26 @@
 import { VIEWS } from '../../app/contexts'
 import { useView } from '../../app/hooks'
 import { PlusIcon } from '../../components/Icons'
+import { useProduct } from '../hooks'
 
 import styles from './ListProducts.module.css'
 
 export const ListProducts = () => {
   const { changeView } = useView()
+  const { products } = useProduct()
   return (
     <section className={styles.listProductsContainer}>
       <button className={styles.listItemContainer} onClick={() => changeView(VIEWS.ADD_PRODUCT)}>
         <p>Nuevo elemento</p>
         <PlusIcon />
       </button>
-      <button className={styles.listItemContainer}>
-        <h4>Pan</h4>
-      </button>
+      {
+        products.map(({ id, name }) => (
+          <button key={id} className={styles.listItemContainer}>
+            <h4>{name}</h4>
+          </button>
+        ))
+      }
     </section>
   )
 }
