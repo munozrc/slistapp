215fb1ccff3e019007a5a11f73f6d27e0d700ed0
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-02-24 20:43:10 -0500

    feat: Create primary variant to button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,7 +3,7 @@ import { ButtonHTMLAttributes } from 'react'
 import styles from './Button.module.css'
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  variant?: string
+  variant?: 'flat' | 'primary'
 }
 
 export const Button = ({ variant = 'flat', children, ...props }: ButtonProps) => {
